3a4fb7093e3f6aca0b17e7d5422287bec2fcc8d5
Jeff Browning <browningjeff1@gmail.com>
2020-07-07 12:09:36 -0500

    no longer needed

--- src/components/script.component.jsx
+++
@@ -1,6 +0,0 @@
-export const appendScript = (scriptToAppend) => {
-    const script = document.createElement("script");
-    script.src = scriptToAppend;
-    script.async = true;
-    document.body.appendChild(script);
-}
