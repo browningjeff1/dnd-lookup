721c0b6ca582b515efd71768cd21c3f5297c37bf
Jeff Browning <browningjeff1@gmail.com>
2020-07-07 16:18:14 -0500

    formating

--- src/components/content.component.jsx
+++ src/components/content.component.jsx
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import Search from './search.component.jsx';
 import Sidebar from './sidebar.component.jsx';
-import '../css/sidebar.css';
-import '../css/search.css';
+import '../css/content.css';
 
 
 export default class Content extends Component {
-    
-
-    render() {
-        return (
-            <main className="content">
-                <Search />
-                <Sidebar />
-                <script src="../js/search.js" async></script>
-            </main>
-        )
-    }
+  render() {
+    return (
+      <main className="content">
+        <Search />
+        <Sidebar />
+      </main>
+    )
+  }
 }
