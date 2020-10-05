a1fa8dbb2fd0794b9e2c1f1763c4f9fc9abdfc35
jugsomilk <58952018+jugsomilk@users.noreply.github.com>
2020-10-05 15:06:47 -0500

    Refactored Races Component Code

--- src/components/races.component.jsx
+++ src/components/races.component.jsx
@@ -1,127 +1,84 @@
 import React, {Component} from 'react';
 import '../css/races.css'
+import { api } from '../functions/ApiCall';
+
 export default class Races extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      speed: Number,
-      ability_bonuses: [],
-      alignment: '',
-      age: '',
-      size: '',
-      size_description: '',
-      starting_proficiencies: [],
-      starting_proficiency_options: [],
-      languages: '',
-      language_desc: '',
-      traits: [],
-      trait_options: [],
-      subraces: [],
-      url: this.props.url
+      data: null
     }
   }
 
-  componentDidMount() {
-    fetch('https://www.dnd5eapi.co' + this.state.url)
-      .then((results) => {
-        return results.json();
-      })
-      .then((data) => {
-        console.log(data)
-        let name = data.name;
-        let speed = data.speed;
-        let ability_bonuses = data.ability_bonuses.map((e) => {
-          return (
-            <div>{ e.name } : + { e.bonus }</div>
-          )
-        })
-        let alignment = data.alignment;
-        let age = data.age;
-        let size = data.size;
-        let size_description = data.size_description;
-        let starting_proficiencies = data.starting_proficiencies.length ? <div className="border"><strong>Starting Proficiencies:</strong>
-          {data.starting_proficiencies.map((e) => {
-            return (
-              <div>
-                <div>{ e.name }</div>
-              </div>
-            )
-          })}
-        </div> : null;
-        let starting_proficiency_options = data.starting_proficiency_options ? <div className="border">Choose from { data.starting_proficiency_options.choose }
-          <ul>{ data.starting_proficiency_options.from.map((e) => {
-            return <li>{e.name}</li>
-          }) }</ul>
-        </div> : null
-        let languages = <ul>
-          { data.languages.map((e) => {
-            return (
-                <li>{ e.name }</li>
-            )
-          }) }
-        </ul>
-        let language_desc = data.language_desc ? data.language_desc : null;
-        let traits = <ul>
-          {data.traits.map((e) => {
-            return(
-              <li>{ e.name }</li>
-            )
-          })}
-        </ul>
-        let subraces = data.subraces.length ? <div className="border"><strong>Subraces: </strong>
-          <ul>
-            {data.subraces.map((e) => {
-              return (
-                <li>{ e.name }</li>
-              )
-            })}
-          </ul>
-        </div> : null;
+  async componentDidMount() {
+    let data = await api.ApiCall(this.props.url)
 
-        this.setState({
-          name: name,
-          speed: speed,
-          ability_bonuses: ability_bonuses,
-          alignment: alignment,
-          age: age,
-          size: size,
-          size_description: size_description,
-          starting_proficiencies: starting_proficiencies,
-          starting_proficiency_options: starting_proficiency_options,
-          languages: languages,
-          language_desc: language_desc,
-          traits: traits,
-          subraces: subraces,
-        })
-      })
-      .catch((err) => {
-        console.error('Error: ', err)
-      })
+    this.setState({
+      data: data
+    })
   }
 
   render() {
+    let { data } = this.state;
     return(
-      <div className='races-container'>
-        <div className='name-container'><div>
-          { this.state.name }
-        </div></div>
-        <div className="border"><strong>Speed: </strong>{ this.state.speed.toString() }</div>
-        <div className="border"><strong>Ability Bonuses: </strong>{ this.state.ability_bonuses }</div>
-        <div className="border"><strong>Alignment: </strong>{ this.state.alignment }</div>
-        <div className="border"><strong>Age: </strong>{ this.state.age }</div>
-        <div className="border"><strong>Size: </strong>{ this.state.size }</div>
-        <div className="border">{ this.state.size_description }</div>
-        <div>{ this.state.starting_proficiencies }</div>
-        <div>{this.state.starting_proficiency_options}</div>
-        <div className="border"> <strong>Languages: </strong> 
-          { this.state.languages }
-        </div>
-        <div className="border">{ this.state.language_desc }</div>
-        <div className="border"><strong>Traits: </strong>
-          { this.state.traits }
-        </div>
-        { this.state.subraces }
+      <div>
+        {data && <div className='races-container'>
+          <div className='name-container'>
+            { data.name }
+          </div>
+          <div className="border"><strong>Speed: </strong>{ data.speed.toString() }</div>
+          <div className="border"><strong>Ability Bonuses: </strong>{ data.ability_bonuses.map((e) => {
+            return (
+              <div key={e.index}>{ e.name } : + { e.bonus }</div>
+            )
+          }) }</div>
+          <div className="border"><strong>Alignment: </strong>{ data.alignment }</div>
+          <div className="border"><strong>Age: </strong>{ data.age }</div>
+          <div className="border"><strong>Size: </strong>{ data.size }</div>
+          <div className="border">{ data.size_description }</div>
+          <div>{ data.starting_proficiencies.length ? <div className="border"><strong>Starting Proficiencies:</strong>
+            {data.starting_proficiencies.map((e) => {
+              return (
+                <div key={e.index}>
+                  <div>{ e.name }</div>
+                </div>
+              )
+            })}
+          </div> : null }</div>
+          <div>{ data.starting_proficiency_options ? <div className="border">Choose from { data.starting_proficiency_options.choose }
+            <ul>{ data.starting_proficiency_options.from.map((e) => {
+              return <li key={e.index}>{e.name}</li>
+            }) }</ul>
+          </div> : null }</div>
+          <div className="border"> <strong>Languages: </strong> 
+            <ul>
+              { data.languages.map((e) => {
+                return (
+                    <li key={e.index}>{ e.name }</li>
+                )
+              }) }
+            </ul> 
+          </div>
+          <div className="border">{ data.language_desc ? data.language_desc : null }</div>
+          <div className="border"><strong>Traits: </strong>
+            <ul>
+              {data.traits.map((e) => {
+                return(
+                  <li key={e.index}>{ e.name }</li>
+                )
+              })}
+            </ul>
+          </div>
+          { data.subraces.length ? <div className="border"><strong>Subraces: </strong>
+            <ul>
+              {data.subraces.map((e) => {
+                return (
+                  <li key={e.index}>{ e.name }</li>
+                )
+              })}
+            </ul>
+          </div> : null }
+        </div>}
       </div>
     )
   }
