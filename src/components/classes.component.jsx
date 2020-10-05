ef568ec166b8a18429d759846e673d3ec6a80733
jugsomilk <58952018+jugsomilk@users.noreply.github.com>
2020-10-05 14:34:48 -0500

    Cleaned up code for classes component

--- src/components/classes.component.jsx
+++ src/components/classes.component.jsx
@@ -16,272 +16,106 @@ import Warlock from '../images/classLogos/Warlock.png';
 import Wizard from '../images/classLogos/Wizard.png';
 // import fetchStartingEquipment from './class-functions/classFunctions'
 import AuthService from '../services/auth.service';
+import { api } from '../functions/ApiCall.js';
 
 export default class Class extends Component {
     constructor(props) {
         super(props);
-        this.fetchStartingEquipment = this.fetchStartingEquipment.bind(this);
-        this.fetchFeature.bind(this);
-        this.fetchClassLevels.bind(this);
 
         this.state = {
-            name: '',
-            hit_die: Number,
-            proficiency_choices: [],
-            proficiencies: [],
-            saving_throws: [],
-            starting_equipment: [],
-            class_levels: [],
-            subclasses: [],
             url: this.props.url,
-            starting_equipment_url: '',
-            starting_equipment_choices: [],
-            class_levels_url: '',
-            feature_url: '',
-            features: '',
-            featuresTrue: false,
             classTrue: this.props.classTrue,
-            subclasses_url: '',
             i: Number,
             currentUser: AuthService.getCurrentUser(),
             saved: AuthService.displaySaved(),
-            
+            data: null,
+            startingEquipment: null,
+            classLevels: null,
+            subclasses: null
         }
     }
 
-    fetchFeature = (url) => {
-        fetch('https://www.dnd5eapi.co' + url)
-            .then((results) => {
-                console.log(results)
-                return results.json()
-            })
-            .then((data) => {
-                console.log(data)
-                let features = data.desc;
-
-                this.setState({
-                    features: features,
-                })
-
-                console.log(this.state.features)
-
-                
-            })
-            .catch((error) => {
-                console.error('Error: ', error)
-            })
-
-        return (
-            <div>
-                {this.state.features}
-            </div>
-        )
-    }
-
-    fetchClassLevels = () => {
-        fetch('https://www.dnd5eapi.co' + this.state.class_levels_url)
-            .then((results) => {
-                return results.json();
-            })
-            .then((data) => {
-                
-
-                let class_levels = data.map((level) => {
-                    
-                    return (
-                        <div className="level-container">
-                            <br/>
-                            <div><strong>Level: </strong>{ level.level }</div>
-                            <div><strong>Ability Score Bonuses: </strong>{ level.ability_score_bonuses }</div>
-                            <div><strong>Proficiency Bonus: </strong>{ level.prof_bonus }</div>
-                            <div id="feature-choices">Feature Choices: { level.features.map(feat => {
-            
-                                    return (
-                                        <div>
-                                            <div><strong>{ feat.name }</strong>:</div>
-                                            <div>
-                                                {  
-                                                    <Features url={feat.url} />
-                                                }
-                                            </div>
-                                            <br />
-                                        </div>
-                                    )
-                            })}</div>
-                        </div>
-                    )
-                })
-
-                this.setState({
-                    class_levels: class_levels,
-                })
-            })
-            .catch((error) => {
-                console.error('Error: ', error)
-            })
-    }
-
-
-    fetchStartingEquipment = () => {
-        fetch('https://www.dnd5eapi.co' + this.state.starting_equipment_url)
-            .then((results) => {
-                return results.json();
-            })
-            .then((data) => {
-                console.log(data)
-                let starting_equipment = data.starting_equipment.map((e) =>{
-                    return (
-                        <div>
-                            <br />
-                            <div>{ e.item.name }</div>
-                            <div><strong>Quantity: </strong>{ e.quantity.toString() }</div>    
-                        </div>
-                )})
-
-                let starting_equipment_choices = data.choice_1.map((e)=> {
-                    return (
-                        <div>
-                        <br />
-                            <div>Choose from { e.choose }</div>
-                            <div>
-                                {e.from.map((e) => {
-                                    return(
-                                        <div>
-                                            <div>{e.item.name}</div>
-                                        </div>
-                                        
-                                    )
-                                })}
-                            </div>
-                        </div>
-                    )
-                })
-
-                this.setState({
-                    starting_equipment: starting_equipment,
-                    starting_equipment_choices: starting_equipment_choices,
-                })
-                
-            })
-            .catch((error) => {
-                console.error('Error: ', error)
-            })
-    }
 
     findClassLogo = () => {
-        if (this.state.name ==='Barbarian') {
+        let { data } = this.state
+        if (data.name ==='Barbarian') {
             return(
                 <img className="classLogo" src={Barbarian} alt="class logo" type="image/png" />
             )
-        } else if (this.state.name ==='Bard') {
+        } else if (data.name ==='Bard') {
             return(
                 <img className="classLogo" src={Bard} alt="class logo" type="image/png" />
             )
-        } else if (this.state.name ==='Cleric') {
+        } else if (data.name ==='Cleric') {
             return(
                 <img className="classLogo" src={Cleric} alt="class logo" type="image/png" />
             )
-        } else if (this.state.name ==='Druid') {
+        } else if (data.name ==='Druid') {
             return(
                 <img className="classLogo" src={Druid} alt="class logo" type="image/png" />
             )
-        } else if (this.state.name ==='Fighter') {
+        } else if (data.name ==='Fighter') {
             return(
                 <img className="classLogo" src={Fighter} alt="class logo" type="image/png" />
             )
-        } else if (this.state.name ==='Paladin') {
+        } else if (data.name ==='Paladin') {
             return(
                 <img className="classLogo" src={Paladin} alt="class logo" type="image/png" />
             )
-        } else if (this.state.name ==='Monk') {
+        } else if (data.name ==='Monk') {
             return(
                 <img className="classLogo" src={Monk} alt="class logo" type="image/png" />
             )
-        } else if (this.state.name ==='Ranger') {
+        } else if (data.name ==='Ranger') {
             return(
                 <img className="classLogo" src={Ranger} alt="class logo" type="image/png" />
             )
-        } else if (this.state.name ==='Rogue') {
+        } else if (data.name ==='Rogue') {
             return(
                 <img className="classLogo" src={Rogue} alt="class logo" type="image/png" />
             )
-        } else if (this.state.name ==='Sorcerer') {
+        } else if (data.name ==='Sorcerer') {
             return(
                 <img className="classLogo" src={Sorcerer} alt="class logo" type="image/png" />
             )
-        } else if (this.state.name ==='Warlock') {
+        } else if (data.name ==='Warlock') {
             return(
                 <img className="classLogo" src={Warlock} alt="class logo" type="image/png" />
             )
-        } else if (this.state.name ==='Wizard') {
+        } else if (data.name ==='Wizard') {
             return(
                 <img className="classLogo" src={Wizard} alt="class logo" type="image/png" />
             )
         }
     }
 
-    componentDidMount() {
-        fetch('https://www.dnd5eapi.co' + this.state.url )
-            .then((results) => {  
-                return results.json();
-            })
-            .then((data) => {
-                let name = data.name;
-                let hit_die = data.hit_die;
-                let proficiency_choices = data.proficiency_choices.map((choice) => {
-                    return (
-                        <div>
-                            <div>Choose from {choice.choose}</div>
-                            <div>{ choice.from.map((e) => {
-                            return(
-                                <div>{e.name}</div>
-                            )
-                            }) }</div>
-                            <br/>
-                        </div>
-                        
-                    )
-                })
-                let proficiencies = data.proficiencies.map((pro) => {
-                    return(
-                        <div>{pro.name}</div>
-                    )
-                })
+    async componentDidMount() {
+        let data = await api.ApiCall(this.props.url);
+        console.log(data);
+        let startingEquipment
+        let classLevels
+        let subclasses
 
-                let starting_equipment_url = data.starting_equipment.url;
-                let class_levels_url = data.class_levels.url;
-                let subclasses = data.subclasses.map((e) => {
-                    return (
-                        <div className="subclasses-container">
-                            <Subclass url={ e.url } />
-                        </div>
-                    )
-                });
-                
-                this.setState({
-                    starting_equipment_url: starting_equipment_url,
-                    class_levels_url: class_levels_url,
-                    subclasses: subclasses,
-                })
-                
-                this.fetchStartingEquipment();
+        if (data) {
+            startingEquipment = await api.ApiCall(data.starting_equipment)
+            classLevels = await api.ApiCall(data.class_levels)
+            subclasses = data.subclasses.map((e) => {
+                return (
+                    <div className="subclasses-container">
+                        <Subclass url={ e.url } />
+                    </div>
+                )
+            });
+            
+        }
 
-                this.fetchClassLevels();
-
-                this.setState({
-                    name: name,
-                    hit_die: hit_die,
-                    proficiency_choices: proficiency_choices,
-                    proficiencies: proficiencies,
-                    starting_equipment_url: starting_equipment_url,
-                })
-                
-                this.findClassLogo();
-                this.classNameIcon();
-            })
-            .catch((error) => {
-                console.error('Error: ', error);
-            })    
+        this.setState({
+            data: data,
+            startingEquipment: startingEquipment,
+            classLevels: classLevels,
+            subclasses: subclasses
+        }) 
+        this.findClassLogo();
     }
 
     handleSaveClick = (e) => {
@@ -317,30 +151,93 @@ export default class Class extends Component {
     }
 
     render() {
+        let { data, startingEquipment, classLevels, subclasses } = this.state;
+        
         return (
-            <div id={this.props.id} className="classContainer">
-                {this.button()}
-                <div>
-                    <a href="#proficiency">Proficiencies</a>
-                    <a href="#starting-equipment">Starting Equipment</a>
-                    <a href="#class-levels">Class Levels</a>
-                    <a href="#subclasses"> Subclasses</a>
-                </div>
-                <h3 className="name-container">{ this.state.name }</h3>
-                <div className="logo-container">
-                    {this.findClassLogo()}
-                </div>
-                <p className="border"><strong>Hit Die: </strong>{ this.state.hit_die.toString() }</p>
-                <div className="jumpTarget border" id="proficiency"><strong>Proficiency Choices: </strong>{ this.state.proficiency_choices }</div>
-                <div className="border"><strong>{ this.state.name } is proficient with: </strong>{ this.state.proficiencies }</div>
-                <div className="border">
-                    <div className="jumpTarget" id="starting-equipment"><strong>Starting Equipment: </strong>{ this.state.starting_equipment }</div>
-                    <div>{ this.state.starting_equipment_choices }</div>
-                </div>
-                <div  className="jumpTarget border" id="class-levels">
-                    <strong>Class Levels: </strong>{ this.state.class_levels }  
-                </div>
-                <div className="jumpTarget border" id="subclasses"><strong>Subclasses: </strong>{ this.state.subclasses }</div>               
+            <div>
+                {data && startingEquipment && subclasses && <div id={this.props.id} className="classContainer">
+                    {this.button()}
+                    <div>
+                        <a href="#proficiency">Proficiencies</a>
+                        <a href="#starting-equipment">Starting Equipment</a>
+                        <a href="#class-levels">Class Levels</a>
+                        <a href="#subclasses"> Subclasses</a>
+                    </div>
+                    <h3 className="name-container">{ data.name }</h3>
+                    <div className="logo-container">
+                        {this.findClassLogo()}
+                    </div>
+                    <p className="border"><strong>Hit Die: </strong>{ data.hit_die.toString() }</p>
+                    <div className="jumpTarget border" id="proficiency"><strong>Proficiency Choices: </strong>{ data.proficiency_choices.map((choice) => {
+                        return (
+                            <div>
+                                <div>Choose from {choice.choose}</div>
+                                <div>{ choice.from.map((e) => {
+                                return(
+                                    <div>{e.name}</div>
+                                )
+                                }) }</div>
+                                <br/>
+                            </div> 
+                        )
+                    })}
+                    </div>
+                    <div className="border"><strong>{ data.name } is proficient with: </strong>{ data.proficiencies.map((pro) => {
+                        return(
+                            <div>{pro.name}</div>
+                        )
+                    })}
+                    </div>
+                    <div className="border">
+                        <div className="jumpTarget" id="starting-equipment"><strong>Starting Equipment: </strong>{ startingEquipment.starting_equipment.map((e) =>{
+                            return (
+                                <div>
+                                    <br />
+                                    <div>{ e.equipment.name }</div>
+                                    <div><strong>Quantity: </strong>{ e.quantity.toString() }</div>    
+                                </div>
+                            )}) 
+                        }</div>
+                        <div>{ startingEquipment.starting_equipment_options.map((e)=> {
+                            return (
+                                <div>
+                                <br />
+                                    <div>Choose from { e.choose }</div>
+                                    <div>
+                                        {e.from[0].equipment.name}
+                                    </div>
+                                </div>
+                            )
+                        }) }</div>
+                    </div>
+                    <div  className="jumpTarget border" id="class-levels">
+                        <strong>Class Levels: </strong>{ classLevels.map((level) => {
+                            return (
+                                <div className="level-container">
+                                    <br/>
+                                    <div><strong>Level: </strong>{ level.level }</div>
+                                    <div><strong>Ability Score Bonuses: </strong>{ level.ability_score_bonuses }</div>
+                                    <div><strong>Proficiency Bonus: </strong>{ level.prof_bonus }</div>
+                                    <div id="feature-choices">Feature Choices: { level.features.map(feat => {
+                    
+                                            return (
+                                                <div>
+                                                    <div><strong>{ feat.name }</strong>:</div>
+                                                    <div>
+                                                        {  
+                                                            <Features url={feat.url} />
+                                                        }
+                                                    </div>
+                                                    <br />
+                                                </div>
+                                            )
+                                    })}</div>
+                                </div>
+                            )
+                        }) }  
+                    </div>
+                    <div className="jumpTarget border" id="subclasses"><strong>Subclasses: </strong>{ this.state.subclasses }</div>               
+                </div>}
             </div>
         )
     }
