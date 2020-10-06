9d77fadf7458a298bbf8b19e6bd04976858a691e
jugsomilk <58952018+jugsomilk@users.noreply.github.com>
2020-10-06 13:54:40 -0500

    moved from class component to function component

--- src/components/equipment.component.jsx
+++ src/components/equipment.component.jsx
@@ -1,83 +1,42 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { api } from '../functions/ApiCall';
 
-export default class Equipment extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      equipment_category: '',
-      weapon_category: '',
-      weapon_range: '',
-      category_range: '',
-      gear_category: '',
-      vehicle_category: '',
-      cost: '',
-      damage: [],
-      range: [],
-      weight: '',
-      desc: [],
-      properties: [],
-      url: this.props.url
+export default function Equipment(props) {
+  const [data, setData] = useState(null)
+
+  async function fetchData() {
+    const result = await api.ApiCall(props.url)
+    setData(result)
+  }
+
+  useEffect(() => {
+    const abortController = new AbortController()
+    fetchData()
+    return () => {
+      abortController.abort()
     }
-  }
+  }, [])
 
-  componentDidMount() {
-    fetch('https://www.dnd5eapi.co' + this.state.url)
-      .then((results) => {
-        return results.json();
-      })
-      .then((data) => {
-        let name = data.name;
-        let equipment_category = data.equipment_category.name ? data.equipment_category.name : data.equipment_category;
-        let weapon_category = data.weapon_category;
-        let weapon_range = data.weapon_range;
-        let category_range = data.category_range;
-        let gear_category = data.gear_category;
-        let vehicle_category = data.vehicle_category;
-        let cost = data.cost.quantity.toString() + ' ' + data.cost.unit;
-        let damage = [];
-        data.damage ? damage.push(<div>
-          <div>{ data.damage.damage_dice }</div>
-          <div>{ data.damage.damage_bonus ? data.damage.damage_bonus.toString() : data.damage.damage_bonus }</div>
-          <div>{ data.damage.damage_type.name }</div>
-        </div>) : damage = [];
-        let weight = data.weight.toString();
-        let desc = data.desc;
+  console.log(data);
 
-        this.setState({
-          name: name,
-          equipment_category: equipment_category,
-          weapon_category: weapon_category,
-          weapon_range: weapon_range,
-          category_range: category_range,
-          gear_category: gear_category,
-          vehicle_category: vehicle_category,
-          cost: cost,
-          damage: damage,
-          weight: weight,
-          desc: desc,
-        })
-      })
-      .catch((err) => {
-        console.error('Error: ', err)
-      })
-  }
-
-  render() {
-    return(
-      <div className="equipmentContainer">
-        <strong>{ this.state.name }</strong>
-        <div>{ this.state.equipment_category }</div>
-        <div>{ this.state.gear_category }</div>
-        <div>{ this.state.weapon_category }</div>
-        <div>{ this.state.weapon_range }</div>
-        <div>{ this.state.category_range }</div>
-        <div>{ this.state.vehicle_category }</div>
-        <div>{ this.state.cost }</div>
-        <div>{ this.state.damage }</div>
-        <div>{ this.state.weight }</div>
-        <div>{ this.state.desc }</div>
-      </div>
-    )
-  }
+  return(
+    <div>
+      {data && <div className="equipmentContainer">
+        <strong>{ data.name }</strong>
+        <div>{ data.equipment_category.name ? data.equipment_category.name : null }</div>
+        <div>{ data.gear_category.name }</div>
+        <div>{ data.weapon_category }</div>
+        <div>{ data.weapon_range }</div>
+        <div>{ data.category_range }</div>
+        <div>{ data.vehicle_category }</div>
+        <div>{ data.cost.quantity }</div>
+        <div>{ data.damage }</div>
+        <div>{ data.weight }</div>
+        <div>{ data.desc }</div>
+      </div>}
+    </div>
+  )
 }
+
+console.log();
